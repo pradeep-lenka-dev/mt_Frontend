56fc2ace4ad69ed812a144518ea217506b119ba0
pradeep-lenka-dev <pradeeplenka6@gmail.com>
2024-04-07 13:27:14 +0530

    change memory

---
+++ dist/mt-frontend/browser/chunk-IQMWILTX.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-PP4CJGME.js";import"./chunk-JPQOBB7I.js";import"./chunk-BGRPFHPW.js";export{a as ViewsModule};
--- dist/mt-frontend/browser/chunk-XG7LQ6SS.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-NJQUTNWF.js";import"./chunk-JPQOBB7I.js";import"./chunk-BGRPFHPW.js";export{a as ViewsModule};
