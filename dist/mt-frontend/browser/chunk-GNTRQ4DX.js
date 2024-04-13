4f8b3ff1547f14dccd6ad812f30aaea38d35498d
pradeep-lenka-dev <pradeeplenka6@gmail.com>
2024-04-13 11:23:05 +0530

    change env

---
+++ dist/mt-frontend/browser/chunk-GNTRQ4DX.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-3XZSRDQC.js";import"./chunk-SPRSLQXT.js";import"./chunk-BGRPFHPW.js";export{a as ViewsModule};
--- dist/mt-frontend/browser/chunk-Y6Y24UMA.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-JLAJ2DLM.js";import"./chunk-PQX225UL.js";import"./chunk-BGRPFHPW.js";export{a as ViewsModule};
