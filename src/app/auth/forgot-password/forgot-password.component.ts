1d64a02677ecbe2e465d5a8813abc01080c8fd17
pradeep lenka <pradeeplenka6@gmail.com>
2024-05-24 11:22:56 +0530

    add resetpassword raw code

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,31 +1,46 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component ,OnInit} from '@angular/core';
+import { FormGroup, Validators,FormBuilder, FormControl } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent implements OnInit{
 
-  passwordResetForm:FormGroup
+  passwordResetForm: FormGroup;
+  otpForm : FormGroup;
 
-  constructor(private authservice : AuthService){}
+  isOtp:boolean = false;
+
+
+  constructor(private authservice : AuthService,private formBuilder: FormBuilder){}
+
+  ngOnInit():void{
+    this.passwordResetForm = new  FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    })
+    this.otpForm = new FormGroup({
+      otp:new FormControl('')
+    })
+  }
 
   onSubmit(){
     const newLocal = "your email.";
     console.log(newLocal);
-    
   }
-  SendRestpasswordLink(){
+  SendRestpasswordLink(): void {
+    if (this.passwordResetForm.valid) {
+      this.isOtp = true
+      console.log('Sending password reset link to:', this.passwordResetForm.value.email);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
-    this.authservice.forgetPassword('p').subscribe(
-      (Response)=>{
-
-      },
-      (error)=>{}
-    )
+  verifyOtp(){
 
   }
 
